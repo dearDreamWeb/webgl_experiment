6ce8b28bce332bafe4003b6d114dff2b7b92c6cd
wxb <superwang9527@163.com>
2021-10-27 09:45:41 +0800

    style: 格式化将双引号变成单引号

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState, useEffect, useRef, InputHTMLAttributes } from "react";
+import { useState, useEffect, useRef, InputHTMLAttributes } from 'react';
 import {
   getGl,
   parametric,
   Vector2D,
   triangulation,
   particleAnimation,
-} from "./utils";
-import drawPolygon, { updateFn } from "./utils/drawPolygon";
-import "./app.css";
+} from './utils';
+import drawPolygon, { updateFn } from './utils/drawPolygon';
+import './app.css';
 
 // input组件
 interface IDiyInput extends InputHTMLAttributes<HTMLInputElement> {
@@ -24,7 +24,7 @@ const DiyInput = (props: IDiyInput) => {
   }, [val]);
   return (
     <input
-      type={type || "range"}
+      type={type || 'range'}
       value={val}
       onChange={e => {
         setVal(Number(e.target.value));
@@ -36,44 +36,44 @@ const DiyInput = (props: IDiyInput) => {
 
 const allTypes = [
   {
-    key: "0",
-    type: "polygon",
-    text: "绘制多边形",
+    key: '0',
+    type: 'polygon',
+    text: '绘制多边形',
   },
   {
-    key: "1",
-    type: "para",
-    text: "绘制抛物线",
+    key: '1',
+    type: 'para',
+    text: '绘制抛物线',
   },
   {
-    key: "2",
-    type: "helical",
-    text: "绘制阿基米德螺旋线",
+    key: '2',
+    type: 'helical',
+    text: '绘制阿基米德螺旋线',
   },
   {
-    key: "3",
-    type: "star",
-    text: "绘制星形线",
+    key: '3',
+    type: 'star',
+    text: '绘制星形线',
   },
   {
-    key: "4",
-    type: "quadricBezier",
-    text: "绘制贝塞尔曲线",
+    key: '4',
+    type: 'quadricBezier',
+    text: '绘制贝塞尔曲线',
   },
   {
-    key: "5",
-    type: "triangulation",
-    text: "三角剖分",
+    key: '5',
+    type: 'triangulation',
+    text: '三角剖分',
   },
   {
-    key: "6",
-    type: "particleAnimation",
-    text: "粒子动画",
+    key: '6',
+    type: 'particleAnimation',
+    text: '粒子动画',
   },
 ];
 
 function App(): JSX.Element {
-  const [type, setType] = useState("particleAnimation");
+  const [type, setType] = useState(allTypes[0].type);
   const canvas = useRef<HTMLCanvasElement>(null);
   const [inputValue, setInputValue] = useState<number>(3);
   const [translateX, setTranslateX] = useState<number>(50);
@@ -86,7 +86,7 @@ function App(): JSX.Element {
     if (!canvas.current) {
       return;
     }
-    const gl = canvas.current.getContext("webgl")!;
+    const gl = canvas.current.getContext('webgl')!;
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     canvas.current.onmousemove = null;
@@ -102,7 +102,7 @@ function App(): JSX.Element {
           t => 0.05 * t,
           t => 0.05 * t ** 2
         );
-        para(-10, 10, 100000).draw("");
+        para(-10, 10, 100000).draw('');
         break;
       case allTypes[2].type:
         const helical = parametric(
@@ -111,7 +111,7 @@ function App(): JSX.Element {
           (t, l) => l * t * Math.sin(t)
         );
 
-        helical(0, 50, 500, 5).draw("helical");
+        helical(0, 50, 500, 5).draw('helical');
         break;
       case allTypes[3].type:
         const star = parametric(
@@ -120,7 +120,7 @@ function App(): JSX.Element {
           (t, l) => l * Math.sin(t) ** 3
         );
 
-        star(0, Math.PI * 2, 500, 5).draw("star");
+        star(0, Math.PI * 2, 500, 5).draw('star');
         break;
       case allTypes[4].type:
         const quadricBezier = parametric(
@@ -139,7 +139,7 @@ function App(): JSX.Element {
           // 绘制30条从圆心出发，旋转不同角度的二阶贝塞尔曲线
           p1.rotate((2 / count) * Math.PI);
           p2.rotate((2 / count) * Math.PI);
-          quadricBezier(0, 1, 100, [p0, p1, p2]).draw("quadricBezier");
+          quadricBezier(0, 1, 100, [p0, p1, p2]).draw('quadricBezier');
         }
         break;
       case allTypes[5].type:
@@ -157,18 +157,18 @@ function App(): JSX.Element {
     if (!canvas.current || type !== allTypes[0].type) {
       return;
     }
-    const gl = canvas.current.getContext("webgl")!;
+    const gl = canvas.current.getContext('webgl')!;
     const program = getGl(gl);
     updateFn(gl, program, inputValue, scale, rotate, translateX, translateY);
   }, [type, inputValue, translateX, translateY, rotate, scale]);
 
   return (
-    <div className="app">
-      <div className="app_nav">
+    <div className='app'>
+      <div className='app_nav'>
         {allTypes.map(item => (
           <button
             key={item.key}
-            className={`${item.type === type ? "nav_active" : ""}`}
+            className={`${item.type === type ? 'nav_active' : ''}`}
             onClick={() => setType(item.type)}
           >
             {item.text}
@@ -176,45 +176,45 @@ function App(): JSX.Element {
         ))}
       </div>
 
-      <div className="canvas_box">
-        <canvas width="800" height="800" ref={canvas}></canvas>
+      <div className='canvas_box'>
+        <canvas width='800' height='800' ref={canvas}></canvas>
         {/* 多边形选项 */}
         {type === allTypes[0].type && (
-          <div className="polygon_box">
-            <div className="polygon_option">
-              <p className="polygon_option_text">边数：</p>
+          <div className='polygon_box'>
+            <div className='polygon_option'>
+              <p className='polygon_option_text'>边数：</p>
               <DiyInput
-                type="text"
+                type='text'
                 value={inputValue}
                 onChangeHandler={val => setInputValue(val)}
                 min={3}
               />
             </div>
-            <div className="polygon_option">
-              <p className="polygon_option_text"> x轴移动：</p>
+            <div className='polygon_option'>
+              <p className='polygon_option_text'> x轴移动：</p>
               <DiyInput
                 value={translateX}
                 onChangeHandler={val => setTranslateX(val)}
               />
             </div>
-            <div className="polygon_option">
-              <p className="polygon_option_text"> y轴移动：</p>
+            <div className='polygon_option'>
+              <p className='polygon_option_text'> y轴移动：</p>
               <DiyInput
                 value={translateY}
                 setValue={setTranslateY}
                 onChangeHandler={val => setTranslateY(val)}
               />
             </div>
-            <div className="polygon_option">
-              <p className="polygon_option_text"> 旋转：</p>
+            <div className='polygon_option'>
+              <p className='polygon_option_text'> 旋转：</p>
               <DiyInput
                 value={rotate}
                 setValue={setRotate}
                 onChangeHandler={val => setRotate(val)}
               />
             </div>
-            <div className="polygon_option">
-              <p className="polygon_option_text"> 缩放：</p>
+            <div className='polygon_option'>
+              <p className='polygon_option_text'> 缩放：</p>
               <DiyInput
                 value={scale}
                 setValue={setScale}
@@ -223,7 +223,7 @@ function App(): JSX.Element {
             </div>
           </div>
         )}
-        {isShowText && "鼠标移入图形中！！！"}
+        {isShowText && '鼠标移入图形中！！！'}
       </div>
     </div>
   );
--- src/utils/drawPolygon.ts
+++ src/utils/drawPolygon.ts
@@ -1,11 +1,18 @@
-export default function drawPolygon(gl: WebGLRenderingContext, program: WebGLProgram, scale: number = 0, rotate: number = 0, translateX = 0, translateY = 0) {
+export default function drawPolygon(
+    gl: WebGLRenderingContext,
+    program: WebGLProgram,
+    scale: number = 0,
+    rotate: number = 0,
+    translateX = 0,
+    translateY = 0
+) {
     const points = new Float32Array([-1, -1, 0, 1, 1, -1]);
     const bufferId = gl.createBuffer();
 
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, points, gl.STATIC_DRAW);
 
-    updateFn(gl, program, 3, scale, rotate, translateX, translateY)
+    updateFn(gl, program, 3, scale, rotate, translateX, translateY);
 }
 
 function getPointArr(a: number[], b: number[], c: number[]): number[] {
@@ -15,7 +22,13 @@ function getPointArr(a: number[], b: number[], c: number[]): number[] {
 /**
  * 得到三角形的点数据
  */
-function getShape(center: number[], r: number, n: number, scale: number = 0, rotate: number = 0): number[] {
+function getShape(
+    center: number[],
+    r: number,
+    n: number,
+    scale: number = 0,
+    rotate: number = 0
+): number[] {
     let idx = 0;
     let x = 0;
     let y = 0;
@@ -31,23 +44,29 @@ function getShape(center: number[], r: number, n: number, scale: number = 0, rot
     }
     let res = []; // 平坦化数组
     for (idx = 0; idx < n; idx++) {
-        res.push(
-            ...getPointArr(pointArr[idx], pointArr[(idx + 1) % n], center)
-        ); // 注意这里是逆时针排布的
+        res.push(...getPointArr(pointArr[idx], pointArr[(idx + 1) % n], center)); // 注意这里是逆时针排布的
     }
     return res;
 }
 
 /**
-* 更新数据
-*/
-export function updateFn(gl: WebGLRenderingContext, program: WebGLProgram, n: number, scale: number, rotate: number, xDistance = 0, yDistance = 0) {
+ * 更新数据
+ */
+export function updateFn(
+    gl: WebGLRenderingContext,
+    program: WebGLProgram,
+    n: number,
+    scale: number,
+    rotate: number,
+    xDistance = 0,
+    yDistance = 0
+) {
     xDistance = (xDistance - 50) / 50;
     yDistance = (yDistance - 50) / 50;
     rotate = rotate * 3.6 * (Math.PI / 180);
     scale = (scale - 50) / 50;
 
-    const vPosition = gl.getAttribLocation(program, "position"); // 获取顶点着色器中的position变量的地址
+    const vPosition = gl.getAttribLocation(program, 'position'); // 获取顶点着色器中的position变量的地址
 
     // 生成N边形
     const data = getShape([0, 0], 0.5, n, scale, rotate);
